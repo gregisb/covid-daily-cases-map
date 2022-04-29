7daf4c7a34688e9e45e881eb9cc03194f75062a6
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-29 11:34:20 -0300

    trying to fix deploy

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+export const supabaseUrl = 'https://fqvqkeojqermirnjqjcy.supabase.co';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
