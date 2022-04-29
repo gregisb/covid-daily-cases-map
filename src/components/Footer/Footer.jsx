c6577100319eefa3836d56d98bcc2605caa3ffaf
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-29 11:29:25 -0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p>Challenge by <a href="https://coodesh.com/" target="_blank">Coodesh</a>. Developed by <a href="https://guilhermeregis.com" target="_blank">Guilherme Regis</a>.</p>
+     
+    </div>
+  )
+}
+
+export default Footer
