447ae6911d82760650ebb94ed284586e4d8f5153
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-29 11:28:24 -0300

    created map chart

---
+++ src/components/MapChart/MapChart.jsx
@@ -0,0 +1,92 @@
+import React, { memo, useContext } from "react";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography
+} from "react-simple-maps";
+import CovidContext from "../../context/CovidContext";
+
+import './MapChart.css'
+import logo from '../../assets/loading.gif' 
+import VariantTable from "../variantTable/VariantTable";
+
+
+
+
+
+const geoUrl = process.env.REACT_APP_GEOURL;
+
+const MapChart = ({ setTooltipContent }) => {
+
+  const { cases, isLoading, customSetCountry } = useContext(CovidContext);
+
+
+  // const filterCountryCases = (CountryName) => {
+
+  //   const countryCases = cases.filter(({location}) => CountryName.includes(location));
+
+  //   const countryData = countryCases.map((country) => (
+  //     `<tr>
+  //       <td>${country.variant}</td>
+  //       <td>${country.total}</td>
+  //     </tr>`
+  //     ))
+
+  //   if (countryCases.length === 0) return 'Sem dados deste país'
+  //   return (
+  //     `<table>
+  //       <tr>
+  //         <th>Variante</td>
+  //         <th>nº casos</th>
+  //       </tr>
+  //       ${countryData.join('')}
+  //     </table>`)
+  // }
+
+  
+  return (
+    <div className='map-chart'>
+      {isLoading && <img className="loading" src={logo} alt="loading..." />}
+      <ComposableMap data-tip="" projectionConfig={{ scale: 150 }}>
+        <ZoomableGroup zoom={1} minZoom={1} maxZoom={2} translateExtent={[[850, 650], [0, 0]]}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME } = geo.properties;
+                    customSetCountry(NAME)
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#746AB0",
+                      outline: "none",
+                      stroke: '#fff',
+                      strokeWidth: '0.3'
+                    },
+                    hover: {
+                      fill: "#333333",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
+  );
+};
+
+export default MapChart;
